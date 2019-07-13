f6a407021594257308b699fefb1eda5ff04bc244
cw <userchenwei@gmail.com>
2019-07-13 16:00:07 +0800

    Local files

--- src/main/scala/WatchDemo.scala
+++ src/main/scala/WatchDemo.scala
@@ -1,4 +1,5 @@
 import java.nio.file._
+import java.util.zip.ZipInputStream
 
 object WatchDemo {
 
@@ -7,20 +8,52 @@ object WatchDemo {
      val watchService:WatchService
       = FileSystems.getDefault.newWatchService
 
+    val workPath: String = "/home/cw/Work"
 
-    val path = Paths.get("/home/cw")
+    //path.get
+
+    val path = Paths.get("/home/cw/Work")
+    val path2 = Paths.get("/home/cw/UnZipFiles")
     path.register(
       watchService,
-      StandardWatchEventKinds.ENTRY_CREATE,
-      StandardWatchEventKinds.ENTRY_DELETE,
-      StandardWatchEventKinds.ENTRY_MODIFY)
+      StandardWatchEventKinds.ENTRY_CREATE)
 
     var key:WatchKey = watchService.take()
 
     while (key != null){
       key.pollEvents().forEach{ x =>
         println(s"Event kind: ${x.kind} . File affected: ${x.context}")
+
+
+        val sourcePath = Paths.get(s"$workPath/${x.context.toString}")
+
+        val zipInputStream = new ZipInputStream(Files.newInputStream(sourcePath))
+        var entry = zipInputStream.getNextEntry
+
+        while (entry != null){
+
+          val toPath = path2.resolve(entry.getName)
+
+
+
+          if (entry.isDirectory) {
+            Files.createDirectory(toPath)
+          }
+          else {
+            Files.createFile(toPath)
+            Files.copy(zipInputStream, toPath)
+          }
+
+          entry = zipInputStream.getNextEntry
+        }
+
+        zipInputStream.close()
+
+
+
+        //if (x.kind().equals(StandardWatchEventKinds.ENTRY_CREATE)) println("Hellow")
       }
+      key.reset()
       key = watchService.take()
     }
 
